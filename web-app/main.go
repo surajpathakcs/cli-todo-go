6125145c272140d274547895865c3742c179689f
npmzero <surajpathak3430@gmail.com>
2025-04-17 23:23:40 +0545

    feat:basic http server with hello route

--- web-app/main.go
+++ web-app/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+func handleHome(w http.ResponseWriter , r *http.Request){
+	fmt.Fprintln(w,"Welcome to our Go Web Api!")
+}
+
+func helloHandler(w http.ResponseWriter , r *http.Request){
+	w.Header().Set("Content-Type","application/json")
+	response := map[string] string{"message":"Hello from the Go!"}
+	json.NewEncoder(w).Encode(response)
+}
+
 func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter , r *http.Request){
-		fmt.Fprintln(w,"Hello There")
-	})
+	http.HandleFunc("/",handleHome)
+	http.HandleFunc("/hello",helloHandler)
 
 	http.ListenAndServe(":8000",nil)
 }
